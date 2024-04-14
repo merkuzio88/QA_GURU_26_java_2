1be958a3ca010b6769e221e2365922f2ca2e5552
Pavel Kulikov <pkulikov@secureducation.ru>
2024-04-14 17:55:40 +0500

    Initial commit

---
+++ src/main/java/ru/merkuzio/Arrays.java
@@ -0,0 +1,171 @@
+package ru.merkuzio;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        ArrayListExample arrayListExample = new ArrayListExample();
+        LinkedListExample linkedListExample = new LinkedListExample();
+        HashSetExample hashSetExample = new HashSetExample();
+        HashMapExample hashMapExample = new HashMapExample();
+
+        arrayListExample.demo();
+        linkedListExample.demo();
+        hashSetExample.demo();
+        hashMapExample.demo();
+    }
+
+    private static class ArrayListExample {
+        private ArrayList<String> list = new ArrayList<>();
+
+        void demo() {
+            System.out.println("ArrayList Example:");
+            add("One");
+            add("Two");
+            add("Three");
+            remove("Two");
+            search("One");
+            search("Two");
+        }
+
+        void add(String element) {
+            list.add(element);
+            System.out.println("Added: " + element);
+            System.out.println("List at the moment: " + list);
+        }
+
+        void remove(String element) {
+            list.remove(element);
+            System.out.println("Removed: " + element);
+            System.out.println("List at the moment: " + list);
+        }
+
+        void search(String element) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).equals(element)) {
+                    System.out.println("Found: " + element);
+                    return;
+                }
+            }
+            System.out.println("Not found: " + element);
+        }
+    }
+
+    private static class LinkedListExample {
+        private LinkedList<String> list = new LinkedList<>();
+
+        void demo() {
+            System.out.println("LinkedList Example:");
+            add("One");
+            add("Two");
+            add("Three");
+            remove("Two");
+            search("One");
+            search("Two");
+        }
+
+        void add(String element) {
+            list.add(element);
+            System.out.println("Added: " + element);
+            System.out.println("List at the moment: " + list);
+        }
+
+        void remove(String element) {
+            list.remove(element);
+            System.out.println("Removed: " + element);
+            System.out.println("List at the moment: " + list);
+        }
+
+        void search(String element) {
+            Iterator<String> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                String item = iterator.next();
+                if (item.equals(element)) {
+                    System.out.println("Found: " + element);
+                    return;
+                }
+            }
+            System.out.println("Not found: " + element);
+        }
+    }
+
+    private static class HashSetExample {
+        private HashSet<String> set = new HashSet<>();
+
+        void demo() {
+            System.out.println("HashSet Example:");
+            add("One");
+            add("Two");
+            add("Three");
+            remove("Two");
+            search("One");
+            search("Two");
+        }
+
+        void add(String element) {
+            set.add(element);
+            System.out.println("Added: " + element);
+            System.out.println("Set at the moment: " + set);
+        }
+
+        void remove(String element) {
+            set.remove(element);
+            System.out.println("Removed: " + element);
+            System.out.println("Set at the moment: " + set);
+        }
+
+        void search(String element) {
+            Iterator<String> iterator = set.iterator();
+            do {
+                String item = iterator.next();
+                if (item.equals(element)) {
+                    System.out.println("Found: " + element);
+                    return;
+                }
+            } while (iterator.hasNext());
+            System.out.println("Not found: " + element);
+        }
+
+    }
+
+    private static class HashMapExample {
+        private HashMap<Integer, String> map = new HashMap<>();
+
+        void demo() {
+            System.out.println("HashMap Example:");
+            add(1, "One");
+            add(2, "Two");
+            add(3, "Three");
+            remove(2);
+            search(1);
+            search(2);
+        }
+
+        void add(Integer key, String value) {
+            map.put(key, value);
+            System.out.println("Added: " + key + " -> " + value);
+            System.out.println("Map at the moment: " + map);
+        }
+
+        void remove(Integer key) {
+            map.remove(key);
+            System.out.println("Removed: " + key);
+            System.out.println("Map at the moment: " + map);
+        }
+
+        void search(Integer key) {
+            for (Integer k : map.keySet()) {
+                if (k.equals(key)) {
+                    System.out.println("Found: " + key + " -> " + map.get(key));
+                    return;
+                }
+            }
+            System.out.println("Not found: " + key);
+        }
+    }
+}
